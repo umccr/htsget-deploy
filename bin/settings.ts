4281a956ec485c1c5ee92a71e2fa4782a8a30f9b
Marko Malenic <mmalenic1@gmail.com>
2025-02-14 15:09:42 +1100

    refactor: update domain name default

--- bin/settings.ts
+++ bin/settings.ts
@@ -4,6 +4,6 @@ import { HtsgetLambdaProps } from "../index";
  * Settings to use for the htsget deployment.
  */
 export const SETTINGS: HtsgetLambdaProps = {
-  domain: "ga4gh.org",
+  domain: "ga4gh-demo.org",
   copyTestData: true,
 };
