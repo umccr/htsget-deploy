c212af151acaff12b5fd0113ca468a3259cf001d
mmalenic <mmalenic1@gmail.com>
2026-01-13 15:10:23 +1100

    build: revert trusted publish in order to rename package

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,8 @@
 import esLint from "@eslint/js";
 import tsEsLint from "typescript-eslint";
+import { defineConfig } from "eslint/config";
 
-export default tsEsLint.config(
+export default defineConfig(
   esLint.configs.recommended,
   tsEsLint.configs.strictTypeChecked,
   tsEsLint.configs.stylisticTypeChecked,
